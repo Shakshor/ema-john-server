4d277621e08a9597ef7f4f1cdc580a5de996acd5
Shakshor <shakshor584@gmail.com>
2022-05-12 16:34:33 +0600

    product with pagination done

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+const app = express();
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+// connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rznlj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('emaJohn').collection('product');
+
+        // load all products
+        app.get('/product', async (req, res) => {
+            console.log('query', req.query);
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+
+            const query = {};
+            const cursor = productCollection.find(query);
+            let products;
+            if (page || size) {
+                // 0 --> skip: 0 get: 0-10(10);
+                // 1 --> skip: 1*10 get: 11-20(10);
+                // 0 --> skip: 2*10 get: 21-30(10);
+                // 0 --> skip: 3*10 get: 31-40(10);
+
+                products = await cursor.skip(page * size).limit(size).toArray();
+            }
+            else {
+                products = await cursor.toArray();
+
+            }
+            res.send(products);
+        })
+
+        // pagination count
+        app.get('/productCount', async (req, res) => {
+            const query = {};
+            // const cursor = productCollection.find(query);
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({ count });
+        })
+
+        // use post to get products by ids/keys
+        app.post('/productByKeys', async (req, res) => {
+            const keys = req.body;
+            const ids = keys.map(id => ObjectId(id));
+            const query = { _id: { $in: ids } }
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            console.log(keys);
+            res.send(products);
+        })
+
+    }
+    finally {
+
+    }
+}
+
+// calling the run function
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Ema John is running');
+});
+
+app.listen(port, () => {
+    console.log('Listening to the port', port);
+});
